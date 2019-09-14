e88857c8053e3ddcbee185eb71a77d272c1cda82
Falendika <falendikategar@gmail.com>
2019-09-14 15:37:12 +0700

    first Commit

---
+++ app/src/main/java/com/example/realtimedatabase/Users.kt
@@ -0,0 +1,10 @@
+package com.example.realtimedatabase
+
+import android.service.voice.VoiceInteractionSession
+
+class Users (var id: String,var nama: String, var email: String){
+
+    constructor():this("","",""){
+
+    }
+}
